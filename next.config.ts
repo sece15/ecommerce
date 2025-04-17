4f14316cdef7666ffac62340cb06e5c5d23706d6
andrealvaradocampos <andrealvaradocampos@gmail.com>
2025-04-17 03:08:56 +0000

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "3000-idx-ecommerce-1744856396231.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev"
+  ],
+};
+
+export default nextConfig;
